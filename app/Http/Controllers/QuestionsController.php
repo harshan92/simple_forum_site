7b87aba77cdd4ef7191849bf08e2d4cd637e84e9
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2019-12-22 01:05:57 +0530

    Added author info and question created dates

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -14,9 +14,11 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-      $questions=Question::latest()->paginate(5);
-      // dd($questions);
-      return view('questions.index',compact('questions'));
+      // \DB::enableQueryLog();
+      $questions=Question::with('user')->latest()->paginate(10);
+      // view('questions.index',compact('questions'))->render();
+       return view('questions.index',compact('questions'));
+      // dd(\DB::getQueryLog());
     }
 
     /**
