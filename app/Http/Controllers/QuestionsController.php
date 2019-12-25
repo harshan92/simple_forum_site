0d713e5be05414f63c8727868af3100d9e2e226f
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2019-12-26 00:18:45 +0530

    Implement the validation rules and store question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Question;
+use App\Http\Requests\AskQuestionRequest;
 use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
@@ -38,9 +39,10 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $request->user()->questions()->create($request->only('title','body'));
+        return redirect()->route('questions.index')->with('success','Your question has been submitted');
     }
 
     /**
