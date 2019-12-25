0ebdac665cc31b315843b20d101a712bc2de2607
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2019-12-26 00:18:45 +0530

    Implement the validation rules and store question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -19,7 +19,7 @@
                     @csrf
                     <div class="form-group">
                       <label for="question-title">Question Title</label>
-                      <input type="text" name="title" id="question-title" class="form-control {{$errors->has('title') ? 'is-invalid' : ''}}">
+                      <input type="text" name="title" id="question-title" value="{{old('title')}}" class="form-control {{$errors->has('title') ? 'is-invalid' : ''}}">
                       @if($errors->has('title'))
                       <div class="invalid-feedback">
                         <strong>{{$errors->first('title')}}</strong>
@@ -28,7 +28,7 @@
                     </div>
                     <div class="form-group">
                       <label for="question-body">Explain your question</label>
-                      <textarea  name="body" rows="10" id="question-body" class="form-control {{$errors->has('body') ? 'is-invalid' : ''}}"></textarea>
+                      <textarea  name="body" rows="10" id="question-body" class="form-control {{$errors->has('body') ? 'is-invalid' : ''}}">{{old('body')}}</textarea>
                       @if($errors->has('body'))
                       <div class="invalid-feedback">
                         <strong>{{$errors->first('body')}}</strong>
