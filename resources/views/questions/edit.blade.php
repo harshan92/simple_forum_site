a14b9890fbd98dc98876f317a9ff4d2c3ac91ec2
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2019-12-29 02:09:24 +0530

    Add question edit funtionality

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">
                   <div class="d-flex align-items-center">
-                    <h3>Ask Question</h3>
+                    <h3>Update Question</h3>
                     <div class="ml-auto">
                       <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to all questions</a>
                     </div>
@@ -15,8 +15,9 @@
                 </div>
 
                 <div class="card-body">
-                  <form class="form" action="{{route('questions.store')}}" method="post">
-                    @include('questions._form',['buttonText'=>'Ask this question'])
+                  <form class="form" action="{{route('questions.update',$question->id)}}" method="post">
+                    {{method_field('PUT')}}
+                    @include('questions._form',['buttonText'=>'Update question'])
                   </form>
                 </div>
             </div>
