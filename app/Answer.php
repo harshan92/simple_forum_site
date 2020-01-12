657d84b03a557790ef092858ad279f292e2a1db5
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-01-12 22:31:02 +0530

    Create answer factory and generate answer

--- app/Answer.php
+++ app/Answer.php
@@ -20,4 +20,13 @@ class Answer extends Model
     {
         return \Parsedown::instance()->text($this->body);
     }
+    public static function boot()
+    {
+        parent::boot();
+        static::created(function ($answer)
+        {
+            $answer->question->increment('answers_count');
+            $answer->question->save();
+        });
+    }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,12 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         factory(App\User::class,3)->create()->each(function($user)
         {
-          $user->questions()->saveMany(factory(App\Question::class, rand(1, 5))->make());
+          $user->questions()->saveMany(factory(App\Question::class, rand(1, 5))->make())
+          ->each(function ($question)
+            {
+              $question->answers()->saveMany(factory(App\Answer::class, rand(1,5))->make());
+            });
+
         });
         // factory(App\Question::class,10)->create();
     }
