d260eddcab820f89ca5a3eefbaa5037037856915
HARSHAN-MAD <harshanmadhurangaw@gmail.com>
2020-02-04 20:43:47 +0530

    Implemented delete functionality

--- app/Answer.php
+++ app/Answer.php
@@ -27,7 +27,11 @@ class Answer extends Model
         static::created(function ($answer)
         {
             $answer->question->increment('answers_count');
-            $answer->question->save();
+        });
+
+        static::deleted(function($answer)
+        {
+            $answer->question->decrement('answers_count');
         });
     }
 
